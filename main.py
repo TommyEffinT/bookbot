a0f5d1c9d521623f77f1e68ed79e605d397054d1
TommyEffinT <tomtp7@hotmail.co.uk>
2025-11-06 16:19:28 +0000

    updated stats and main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def get_book_text(path):
-    with open(path) as f:
-        file_contents = f.read()
-    return file_contents
+    with open(path) as f: # opens file
+        file_contents = f.read() # reads the text
+    return file_contents # returns text to console
 
+# imports functions from stats.py file
 from stats import count_words, character_count
 
 def main():
+
     # relative path, not home directory like shell/bash
     path_to_file = "books/frankenstein.txt"
     book_text = get_book_text(path_to_file)
@@ -13,6 +15,7 @@ def main():
     # --- word count ---
     num_words = count_words(book_text) 
     print(f"Found {num_words} total words")
+
     # --- character count ---
     char_counts = character_count(book_text)
     for char, count in char_counts.items():
--- stats.py
+++ stats.py
@@ -1,11 +1,11 @@
 def count_words(file_contents):
-    words = file_contents.split()
-    return len(words)
+    words = file_contents.split() # splits text into a seperate word strings
+    return len(words) # returns the count of strings
 
 def character_count(file_contents):
-    file_contents = file_contents.lower()
-    # makes everything lowercase
-    chars = {}
+    file_contents = file_contents.lower() # makes all characters lowercase
+    chars = {} 
     for c in file_contents:
         chars[c] = chars.get(c, 0) + 1 
     return chars
+# iterates over every word in text and populates blank dict with both char and count.

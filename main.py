934f1ce02d7310f2903344f5683bcf387e269468
TommyEffinT <tomtp7@hotmail.co.uk>
2025-11-06 15:38:08 +0000

    updated stats and main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def get_book_text(path):
         file_contents = f.read()
     return file_contents
 
-from stats import count_words
+from stats import count_words, character_count
 
 def main():
     path_to_file = "books/frankenstein.txt"
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def count_words(file_contents):
     words = file_contents.split()
     return len(words)
+
+def character_count(file_contents):
+    text = file_contents.lower()
+    # makes everything lowercase
+    chars = {}
+    for c in text:
+        if c in chars:
+            chars[c] += 1
+        else:
+            chars[c] = 1
+    return chars

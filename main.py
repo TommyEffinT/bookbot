441feb67e79ab33d6f4727f62139e319f22961a8
TommyEffinT <tomtp7@hotmail.co.uk>
2025-11-06 15:22:11 +0000

    created stats as a file to import as a function

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ def get_book_text(path):
         file_contents = f.read()
     return file_contents
 
-def count_words(file_contents):
-    words = file_contents.split()
-    return len(words)
+from stats import count_words
 
 def main():
     path_to_file = "books/frankenstein.txt"

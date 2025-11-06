8abd306379dee15399fcd23bb9d457d4c3e9a985
TommyEffinT <tomtp7@hotmail.co.uk>
2025-11-06 14:30:28 +0000

    updated main with bookbot code

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+def main():
+    path_to_file = "books/frankenstein.txt"
+    
+    book_text = get_book_text(path_to_file)
+    print(book_text)
+
+    main()

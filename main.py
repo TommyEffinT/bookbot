4131d934c3e42f14925c0ccaf4667a28d44c97eb
TommyEffinT <tomtp7@hotmail.co.uk>
2025-11-06 15:14:56 +0000

    updated main.py with word count

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
     return file_contents
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
 def main():
     path_to_file = "books/frankenstein.txt"
-    
+    # relative path, not home directory like shell/bash
     book_text = get_book_text(path_to_file)
-    print(book_text)
+    num_words = count_words(book_text)
+    print(f"Found {num_words} total words")
 
-main()
+
+
+main() 
